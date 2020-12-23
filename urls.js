e9fe4858de1dedb8413de66465f4a5664801b1c8
Starch <mrdave67@gmail.com>
2020-12-23 21:53:19 +0000

    url updated

--- urls.js
+++ urls.js
@@ -1,4 +1,4 @@
-const WP_URL = "http://192.168.0.31:8888/";
+const WP_URL = "http://wp.thegingerblondie.ie/";
 const EMAIL_FROM = "<starchcode@gmail.com>";
 const EMAIL_TO = "starchcode@gmail.com"; //could be a list: Fred@email.com, Brad@email.com in a string format
 const WP_API_URL = WP_URL + 'wp-json/wp/v2/';
