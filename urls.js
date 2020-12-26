f4395a6ebea5e5561d357aeca5c4edfdc046899d
Starch <mrdave67@gmail.com>
2020-12-26 13:40:25 +0000

    email

--- urls.js
+++ urls.js
@@ -1,6 +1,6 @@
 const WP_URL = "https://wp.thegingerblondie.ie/";
 const EMAIL_FROM = "<starchcode@gmail.com>";
-const EMAIL_TO = "TheGingerBlondieBakery@gmail.com"; //could be a list: Fred@email.com, Brad@email.com in a string format
+const EMAIL_TO = "thegingerblondiebakery@gmail.com"; //could be a list: Fred@email.com, Brad@email.com in a string format
 const WP_API_URL = WP_URL + 'wp-json/wp/v2/';
 
 module.exports = {WP_URL: WP_URL, EMAIL_FROM: EMAIL_FROM,  EMAIL_TO:EMAIL_TO, WP_API_URL: WP_API_URL}
